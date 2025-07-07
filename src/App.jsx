e8ecdac3d8712ecca712f573fdead8c9b8022be6
Complinator <bssaldivia@gmail.com>
2025-07-06 22:36:53 -0400

    Improved navbar links
    Added css and modified the code

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/awards" element={<Awards />} />
           <Route path="/blog" element={<Blog />} />
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,19 +1,20 @@
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
     return(
         <div className="navbar">
-            <div className="navbar-logo">
-                <img src="/src/assets/Logos/Content/Portfolio.svg" alt="Logo" height="22px" />
-                <div className="navbar-title">
-                    <span>DevPortfolio</span>
-                </div>
-            </div>
+            <Link to="/" className="navbar-logo">
+                    <img src="/src/assets/Logos/Content/Portfolio.svg" alt="Logo" height="22px" />
+                    <div className="navbar-title">
+                        <span>DevPortfolio</span>
+                    </div>
+            </Link>
             <div className="navbar-links">
-                <a href="#home">Home</a>
-                <a href="#about">About</a>
-                <a href="#services">Services</a>
-                <a href="#contact">Contact</a>
+                <Link to="/blogs">Blogs</Link>
+                <Link to="/awards">Awards</Link>
+                <Link to="/experience">Experience</Link>
+                <Link to="/projects">Projects</Link>
             </div>
         </div>
     );
