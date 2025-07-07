bad141b7d073572a685b44493cce4beae7d3d7d2
Complinator <bssaldivia@gmail.com>
2025-07-07 00:06:55 -0400

    Finished general navbar structure
    missing smooth animation

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,21 +1,27 @@
+import ImageWidget from "../widgets/Image";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 
 function Navbar() {
     return(
         <div className="navbar">
-            <Link to="/" className="navbar-logo">
-                    <img src="/src/assets/Logos/Content/Portfolio.svg" alt="Logo" height="22px" />
-                    <div className="navbar-title">
-                        <span>DevPortfolio</span>
-                    </div>
-            </Link>
-            <div className="navbar-links">
-                <Link to="/blogs">Blogs</Link>
-                <Link to="/awards">Awards</Link>
-                <Link to="/experience">Experience</Link>
-                <Link to="/projects">Projects</Link>
+            <div className="navbar-content">
+                <Link to="/" className="navbar-logo">
+                        <img src="/src/assets/Logos/Content/Portfolio.svg" alt="Logo" height="22px" />
+                        <div className="navbar-title">
+                            <span>DevPortfolio</span>
+                        </div>
+                </Link>
+                <div className="navbar-links">
+                    <Link to="/blogs">Blogs</Link>
+                    <Link to="/awards">Awards</Link>
+                    <Link to="/experience">Experience</Link>
+                    <Link to="/projects">Projects</Link>
+                </div>
             </div>
+            <Link to="/about" className="navbar-image">
+                <ImageWidget />
+            </Link>
         </div>
     );
 }
---
+++ src/components/widgets/Image.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Image.css";
+
+function ImageWidget() {
+    return (
+        <div className="image-widget">
+            <img 
+                src="./src/assets/Content/ProfilePicture.jpeg" 
+                alt="Profile" 
+                className="image-widget-circle" 
+            />
+        </div>
+    );
+}
+
+export default ImageWidget;
