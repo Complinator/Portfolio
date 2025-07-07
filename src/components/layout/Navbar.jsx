b028572c55eb5afc1c465caa3f3e472ac10ff7ec
Complinator <bssaldivia@gmail.com>
2025-07-06 21:59:14 -0400

    First Navbar Push
    Most basic navbar structure, still missing out things

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,6 +3,18 @@ import "./Navbar.css";
 function Navbar() {
     return(
         <div className="navbar">
+            <div className="navbar-logo">
+                <img src="/src/assets/Logos/Content/Portfolio.svg" alt="Logo" height="22px" />
+                <div className="navbar-title">
+                    <span>DevPortfolio</span>
+                </div>
+            </div>
+            <div className="navbar-links">
+                <a href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#services">Services</a>
+                <a href="#contact">Contact</a>
+            </div>
         </div>
     );
 }
