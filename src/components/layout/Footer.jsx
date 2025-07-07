b982ab91ef6d48e617d0ca8abf5b9883209e1e64
Complinator <bssaldivia@gmail.com>
2025-07-07 02:01:18 -0400

    Fixed little thing on footer
    I forgot to put the correct link on gmail

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
             <div className="footer-widgets">
                 <Logo svgPath="./src/assets/Logos/Social/Linkedin.svg" text="/bsaldivia" link="https://www.linkedin.com/in/bsaldivia" />
                 <Logo svgPath="./src/assets/Logos/Social/Github.svg" text="@Complinator" link="https://github.com/Complinator" />
-                <Logo svgPath="./src/assets/Logos/Social/Email.svg" text="bssaldivia@gmail.com" link="https://github.com/Complinator" />
+                <Logo svgPath="./src/assets/Logos/Social/Email.svg" text="bssaldivia@gmail.com" link="mailto:bssaldivia@gmail.com?subject=Hello%20from%20your%20portfolio&body=Hi%20Benjamín,%0D%0A%0D%0AI%20visited%20your%20portfolio%20and%20would%20like%20to%20get%20in%20touch.%0D%0A%0D%0ABest%20regards," />
             </div>
             <div className="footer-text">@2025 Benjamín Saldivia. All rights reserved</div>
         </div>
